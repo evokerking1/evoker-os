a9560bade3fe2fc5e48f2f17a23518cf4317d87a
evokerking1 <118303818+evokerking1@users.noreply.github.com>
2024-12-28 15:42:11 -0500

    Got it to print to screen whitch lead to me having to tweak some stuff but else it works.  Also made a updated build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,22 @@
 echo "Building kernel..."
 
-cargo build --target x86_64-evokerking_os.json
+cargo bootimage
+
+echo "Built Kernel!"
+
+echo "Loading Kernel in QEMU."
+echo "Loading Kernel in QEMU.."
+echo "Loading Kernel in QEMU..."
+
+echo "NOTE: You will not be able to use terminal until qemu is closed!"
+
+echo "Running Kernel in QEMU"
+
+qemu-system-x86_64 -drive format=raw,file=target/x86_64-evokerking_os/debug/bootimage-evoker-os.bin
+
+
+echo "QEMU Closed."
+
+echo "Ran Kernel in QEMU"
+
+echo "Terminal is now usable."
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![no_std]
 #![no_main]
 
-mod vga_buffer;
 
 use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Evokerking.";
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -12,5 +13,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
